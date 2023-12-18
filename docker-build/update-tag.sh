fcab4167116be7fdbb8fbcd65d75ebe753833acd
dnoronh <delnor10@yahoo.com>
2023-12-18 23:12:42 +0530

    changes

--- docker-build/update-tag.sh
+++ docker-build/update-tag.sh
@@ -3,11 +3,13 @@ new_tag=$1
 cd /tmp
 git clone https://${GITHUB_TOKEN}@github.com/dnoronh/ruby-app-gitops-repo.git
 cd ruby-app-gitops-repo/deployment/ruby-app
+
 cur_tag=$(cat ./values.yaml | grep tag: | awk '{print $2}')
-echo $cur_tag
-echo $new_tag
+echo "Old image tag: $cur_tag"
+echo "New image tag: $new_tag"
 sed -i "s/tag: $cur_tag/tag: \"$new_tag\"/" ./values.yaml
-git add -A
-git commit -m "Update image version to $new_tag"
-git push origin
+
+echo "Commit and push changes to repo - ruby-app-gitops-repo"
+git add -A && git commit -m "Update image version to $new_tag"
+git push origin --quiet
 rm -rf /tmp/ruby-app-gitops-repo
